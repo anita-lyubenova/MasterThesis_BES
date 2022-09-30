71c2c04aaf71c54335a9178bea7d320aeb1ec280
anita-lyubenova <anitaencheva@gmail.com>
2022-09-30 10:38:57 +0200

    Sim2 for mean=median=mode
    the performance of BES is not  impaired, i.e. the well powered studies compensate for the underpowered ones

--- Scripts/Sim2.R
+++ Scripts/Sim2.R
@@ -1,11 +1,7 @@
-# Research Question:
-# Given that all studies in the study set have the same power level (eta) to
-# support the true hypothesis , what is the minimum power level eta 
-# needed ao that the overall power of BES (zeta) is acceptable?
+#Research Question:
+# Given that the average power is fixed, how does the distribution of power across the studies in 
+# the sear affect the performance (or maybe power) of BES?
 
-#where
-#eta = P(BFic > 1| Hi) = P(BFci > 1 | Hc)  (power in indiviudal studies)
-#zeta = P(PMPaggr.ic > thres | Hi) = P(PMPaggr.ci < 1-thres | Hc) (BES-power)???
 
 library(beepr)
 library(tidyverse)
@@ -24,34 +20,27 @@ library(mvtnorm)
 library(highcharter)
 library(lavaan)
 library(writexl)
-#
+
+
 
 #load the functions
 load("Outputs/functions.RData")
 
+
 #load the sample sizes used for the simulation
-planned.n<-read_xlsx("Simulations planning.xlsx", sheet = "Sim1")
+planned.n<-read_xlsx("Simulations planning.xlsx", sheet = "Sim2")
+planned.n$total.n <- apply(planned.n[3:12], 1, sum)
 
 
-# Simulation 1 -----------------------------------------------------------
-## k=2 -------------
-# size of the study set: T= 10
-# power levels: c(.50, .60, .70, .80, .90, .95, .99) [to obtain BFic > 1 for the correct hypothesis (Hi or Hc) ]
 
-#Research Question: Given that all studies have the same power to support the true hypothesis over the complement,
-#                   what power level is enough to produce reliable BES-aggregate support?
-
+# Simulation 2 ---------------
+models <- c("normal")
+pcor <- c(0.2)
 r2<-.04
 ratio_beta <- c(2,1)
-
 coefs(r2, ratio_beta, cormat(pcor, length(ratio_beta)), "normal")
-pcor <- c(0.2)
-models <- c("normal")
 hypothesis<-"V1 > V2"
-
 complement<-TRUE
-
-set.seed(123)
 
 iter<-1000
 
@@ -60,16 +49,14 @@ column.names<-c(paste0("Study.", seq(1:10)), "log.aggr.BF", "aggr.PMP")
 slice.names<-paste0("Condition.", seq(1:nrow(planned.n)))
 
 BFic<-array(NA, dim = c(iterations=iter, studies=length(column.names), conditions=nrow(planned.n)),
-                  dimnames = list(row.names,column.names, slice.names))
-
-scatterp.BFic<-list()
+            dimnames = list(row.names,column.names, slice.names))
 
 seed<-123
 
 #for each condition (manipulated sample size distribution in the set of studies) (slice m in the array)
 for(m in 1:nrow(planned.n)){
   
-  n<-planned.n[m, 3:12] %>% as.numeric()
+  n<-planned.n[1, 3:12] %>% as.numeric()
   
   #for each iteration (row i in the array)
   for(i in 1:iter) {
@@ -124,7 +111,7 @@ correct.aggr<-vioplot.ic.df %>%
   summarize(correct.75 = sum(aggr.PMP>.75)/iter,
             correct.90 = sum(aggr.PMP>.90)/iter,
             correct.95 = sum(aggr.PMP>.95)/iter
-            )
+  )
 
 
 
@@ -141,7 +128,7 @@ vioplot.ic<-vioplot.ic.df %>%
   labs(
     x = "Condition",
     y = "aggregate PMP",
-    title = paste("Distribution of aggregate PMPs from 10 studies with equal power (eta) when testing Hi against Hc across", iter, "iterations"),
+    title = paste("Distribution of aggregate PMPs from a set of 10 studies with the same average power (0.7, total N =", sum(planned.n) ,") when testing Hi against Hc across", iter, "iterations"),
     subtitle = "Each point represents an aggregate PMP from 10 studies from one iteration"
   )+ 
   # Customizations
@@ -152,8 +139,8 @@ vioplot.ic<-vioplot.ic.df %>%
   )+ 
   geom_hline(yintercept=c(0.75, 0.90, 0.95), linetype="dashed", 
              color = "red", size=0.8)+
-  scale_x_discrete(labels=paste(paste("eta =", planned.n$power,"\n"), "n = ", planned.n$Study.1)
-                   )+
+  scale_x_discrete(labels=paste("eta.avg =", planned.n$power,"\n")
+  )+
   annotate("label",
            x = seq(1:nrow(planned.n))+0.3,
            y = rep(c(0.77), times=nrow(planned.n)),
@@ -175,81 +162,6 @@ vioplot.ic
 
 
 
-## k=3 -------------
-power.lvls<-c(seq(from=0.5, to=0.95, by=0.05),0.99)
 
-
-r2<-.13
-ratio_beta <- c(3,2,1)
-pcor <- c(0.2)
-coefs(r2, ratio_beta, cormat(pcor, length(ratio_beta)), "normal")
-models <- c("normal")
-hypothesis<-"V1 > V2 > V3"
-complement<-TRUE
-
-power.k3$n 
-planned.n<-data.frame(Condition=1:length(power.lvls),
-                      power=power.lvls,
-                      Study.1 = power.k3$n,
-                      Study.2 = power.k3$n,
-                      Study.3 = power.k3$n,
-                      Study.4 = power.k3$n,
-                      Study.5 = power.k3$n,
-                      Study.6 = power.k3$n,
-                      Study.7 = power.k3$n,
-                      Study.8 = power.k3$n,
-                      Study.9 = power.k3$n,
-                      Study.10 = power.k3$n
-                      )
-set.seed(123)
-
-iter<-1000
-
-row.names<-paste0("Iter.", seq(1:iter))
-column.names<-c(paste0("Study.", seq(1:10)), "log.aggr.BF", "aggr.PMP")
-slice.names<-paste0("Condition.", seq(1:nrow(planned.n)))
-
-BFic<-array(NA, dim = c(iterations=iter, studies=length(column.names), conditions=nrow(planned.n)),
-            dimnames = list(row.names,column.names, slice.names))
-
-seed<-123
-
-#for each condition (manipulated sample size distribution in the set of studies) (slice m in the array)
-for(m in 1:nrow(planned.n)){
-  
-  n<-planned.n[m, 3:12] %>% as.numeric()
-  
-  #for each iteration (row i in the array)
-  for(i in 1:iter) {
-    
-    #for each study; column s in the array
-    for(s in 1:length(n)){
-      
-      seed=seed+1
-      set.seed(seed)
-      
-      print(paste("Condition m:", m, ", Iteration i:", i, "Study s:", s))
-      
-      BF<-gen_dat(r2=r2, 
-                  betas=coefs(r2, ratio_beta, cormat(pcor, length(ratio_beta)), "normal"),
-                  rho=cormat(pcor, length(ratio_beta)),
-                  n=n[s],
-                  "normal")%$%
-        lm(Y ~ V1 + V2 + V3) %>%
-        BF(hypothesis = hypothesis, complement = complement) %$%
-        BFtable_confirmatory %>% as.data.frame()%$% BF
-      
-      BFic[i,s,m]<-BF[1]/BF[2] 
-      
-      
-    }# end iterations loop i
-    
-    #after all 10 studies in the set were simulated and evaluated in iteration i => calculate the log aggregate BF for iteration i
-    BFic[i,11,m] <-sum(log(BFic[i,1:10,m]))
-    BFic[i,12,m] <- prod(BFic[i,1:10,m])/(prod(BFic[i,1:10,m]) + 1)
-    
-  }# end study loop s
-  
-}#end conditions loop; THE END
 
 
