a82096aac4d8caf35f3ddcaa9b42023554dcbec7
anita-lyubenova <anitaencheva@gmail.com>
2022-11-06 18:44:41 +0100

    approximated the fit with a beta density depending on the ES q

--- Scripts/can I model the fit of Hi.R
+++ Scripts/can I model the fit of Hi.R
@@ -178,11 +178,24 @@ gg.df<-sim1.f1[,,1] %>%
          ES=as.factor(ES)) %>% 
   filter(q<0.37) 
 
+gg.df<-sim1.f1[,,1] %>% 
+  as.data.frame() %>% 
+  pivot_longer(cols = everything(),
+               names_to = "ES",
+               values_to = "fit"
+  ) %>%
+  mutate(q=rep(q, times=10000),
+         ES=as.factor(ES)
+         ) %>% 
+  filter(q<.29)
 
-gg.hist<-ggplot(gg.df,aes(x=fit))+
-        geom_histogram(bins=50,colour="black",size=0.2, fill="gray")+
-        facet_wrap(~ES,ncol=5, nrow=5)+
+gg.hist<-ggplot()+
+        geom_histogram(data=gg.df,bins=50,colour="black",size=0.2, fill="gray",
+                       mapping=aes(x=fit,y=..density..))+
+        geom_density()+
+        facet_wrap(~ES,ncol=5, nrow=3)+
         theme_minimal()
+gg.hist
 
 
 
@@ -223,6 +236,9 @@ gg.hist<-ggplot(gg.df,aes(x=fit))+
   
 
 
+
+
+## -----------------------
 
 #Beta density exploration (alpha and beta) --------------------
 #Goal: find alpha and beta coefficients that are a function of ES and n such that 
@@ -266,6 +282,118 @@ for(s in 1:length(beta.par)){
        )
   
 }
+
+## Density df ------------------
+x<-seq(from=0.01, to=0.99, by=0.01)
+dens.df<-matrix(NA, nrow=length(x), ncol = length(q), dimnames = list(x,q))
+n<-632
+
+for(i in 1:ncol(dens.df)){
+  dens.df[,i]<-dbeta(x, 1+q[i], 1-q[i])
+}
+
+for(i in 1:ncol(dens.df)){
+  dens.df[,i]<-dbeta(x, 1+q[i]*n/100, exp(q[i]))
+}
+
+
+alpha<-c()
+beta<-c()
+for(i in 1:ncol(dens.df)){
+  alpha[i]=1+q[i]*n/100
+  beta[i]=q[i]/(1+exp(q[i]))
+  dens.df[,i]<-dbeta(x, alpha[i], beta[i])
+}
+
+compare.densities(a="1+q*n/100", b="1/(1+6*q)")
+
+#gg.dens.df<-dens.df %>% 
+  as.data.frame() %>%
+  tibble::rownames_to_column(var="x") %>% 
+  pivot_longer(cols = !contains("x"),
+               names_to = "q",
+               values_to = "likelihood"
+  )  %>% 
+  mutate(n=n,
+         q=as.numeric(q),
+         alpha=1+q*n/100,
+         beta=1-q) %>% 
+  filter(q<.29)
+
+#overlay observed density with beta density
+ggplot()+
+  geom_density(data=gg.df,mapping=aes(x=fit))+
+  facet_wrap(~ES,ncol=5, nrow=3)+
+  theme_minimal()+
+  geom_line(data=gg.dens.df,
+            mapping=aes(x=as.numeric(x),
+                        y=likelihood,
+                        group=as.factor(q),
+                        colour="red"))+
+  geom_text(data=gg.dens.df,
+            mapping=aes(label=paste("a=",round(alpha,2), "\nb=",round(beta,2)), x=0.3, y=13),
+            size=3)+
+  scale_y_continuous(limits = c(0,15))+
+  facet_wrap(~as.factor(q),ncol=5, nrow=3)
+
+#function to plot the observed vs. beta density
+compare.densities<-function(a,b, filter.q="q<.29"){
+  gg.df<-sim1.f1[,,1] %>% 
+    as.data.frame() %>% 
+    pivot_longer(cols = everything(),
+                 names_to = "ES",
+                 values_to = "fit"
+    ) %>%
+    mutate(q=rep(q, times=10000),
+           ES=as.factor(ES)
+    ) %>% 
+    filter(eval(parse(text=filter.q)))
+  
+  alpha<-c()
+  beta<-c()
+  for(i in 1:ncol(dens.df)){
+    alpha[i]=eval(parse(text=str_replace_all(a, "q", "q[i]")))
+    beta[i]=eval(parse(text=str_replace_all(b, "q", "q[i]")))
+    dens.df[,i]<-dbeta(x, alpha[i], beta[i])
+  }
+  
+ 
+  gg.dens.df<-dens.df %>% 
+    as.data.frame() %>%
+    tibble::rownames_to_column(var="x") %>% 
+    pivot_longer(cols = !contains("x"),
+                 names_to = "q",
+                 values_to = "likelihood"
+    )  %>% 
+    mutate(n=n,
+           q=as.numeric(q),
+           alpha=eval(parse(text=a)),
+           beta=eval(parse(text=b))) %>% 
+    filter(eval(parse(text=filter.q)))
+  
+  #overlay observed density with beta density
+  ggplot()+
+    geom_histogram(data=gg.df,bins=50,colour="grey",size=0.2, fill="lightgray",
+                   aes(x=fit,y=..density..))+
+    geom_density(data=gg.df,mapping=aes(x=fit))+
+    facet_wrap(~ES)+
+    theme_minimal()+
+    geom_line(data=gg.dens.df,
+              mapping=aes(x=as.numeric(x),
+                          y=likelihood,
+                          group=as.factor(q),
+                          colour="red"))+
+    geom_text(data=gg.dens.df,
+              mapping=aes(label=paste("a=",round(alpha,2), "\nb=",round(beta,2)), x=0.3, y=13),
+              size=3)+
+    scale_y_continuous(limits = c(0,15))+
+    facet_wrap(~as.factor(q))
+  
+}
+
+compare.densities(a="1+q*n/100", b="1/(1+n/100*q)", "q<.40")
+
+compare.densities(a="1+q*n/100", b="1/(1+100*q^(2.2+q*1.76))", "0.30<q & q<.40")
 
 
 #Beta density exploration (phi and k) ------------------------
