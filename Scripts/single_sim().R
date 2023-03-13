35215fecaddd95f238a8d5e94e3f5ad033e301af
anita-lyubenova <anitaencheva@gmail.com>
2023-03-13 20:02:59 +0100

    checking for admissible heterogeneity in H1: b1>b2>b3

--- Scripts/single_sim().R
+++ Scripts/single_sim().R
@@ -5,12 +5,32 @@
 # R2
 # pcor : correlation between the predictors
 # p : number of predictors
-# hypothesis : a string with the hypotheses of interest; must include all predictors
+# hypothesis : a string with the hypotheses of interest; must be in the format of bain()
 # n : sample size 
 # model : "linear", #linear, logistic or probit regression
 
-# pop_ratio_beta : defines the truth in the population; the ratio beta1:beta2:beta3 as a numeric vector, where larger values indicate larger parameter value
-#
+# ~~pop_ratio_beta ~~ : defines the truth in the population; the ratio beta1:beta2:beta3 as a numeric vector, where larger values indicate larger parameter value
+# betas : a numeric vector with the beta coefficients;  defines the truth in the population;
+# sigma_beta: variance covariance matrix of the (true) regression parameters - can be used to induce heterogeneity
+# iter: number of iterations
+single_sim<-function(R2,  # R-squared of the regression model
+                     pcor,  # correlation between the predictors
+                     betas,  # a numeric vector with the beta coefficients;  defines the truth in the population;
+                     Sigma_beta,  # variance covariance matrix of the (true) regression parameters - can be used to induce heterogeneity
+                     hypothesis,  # a string with the hypotheses of interest; must be in the format of bain()
+                     n,  #sample size 
+                     model,  #linear, logistic or probit regression
+                     iter   #number of iterations
+){
+  
+    
+}
+
+
+
+
+
+
 
 
 
@@ -20,24 +40,28 @@
 source("scripts/ThomVolker scripts/functions.R")
 r2<-0.13
 pcor<-0.3
-ratio_beta<-c(16,4,1)
+ratio_beta<-c(9,3,1)
 betas<-coefs(r2, ratio_beta, cormat(pcor, length(ratio_beta)), "normal")
 
 
-bcor<-0 #correlation between the coefficients
-sigma_beta<-diag(x=0.10*betas, nrow = length(betas),ncol = length(betas)) #covariance matrix of the coefficients
+# bcor<-0 #correlation between the coefficients
+# sigma_beta<-diag(x=0.10*betas, nrow = length(betas),ncol = length(betas)) #covariance matrix of the coefficients
 
 #How often is b1>b2>b3 for SD being the specified percentage of the coefficients
-percentage=c(seq(0.01, 0.1, by=0.01),seq(0.1, 0.5, by=0.1))
+percentage=c(seq(0.01, 0.09, by=0.01),seq(0.1, 0.9, by=0.1))
 iter=10000
 a<-data.frame(percentage=percentage,
-              prop_correct=NA
+              prop_correct=NA,
+              SDs = NA
               )
+percentage[1]*betas
 for(p in 1:length(percentage)){
 
-    sigma_beta<-diag(x=percentage[p]*betas, nrow = length(betas),ncol = length(betas)) #covariance matrix of the coefficients
+    sigma_beta<-diag(x=(percentage[p]*betas)^2, nrow = length(betas),ncol = length(betas)) #covariance matrix of the coefficients
+    #sigma_beta<-diag(x=percentage[p]*betas)#, nrow = length(betas),ncol = length(betas))
     b<-mvrnorm(n=iter, mu=betas, Sigma = sigma_beta)
     a$prop_correct[p]<- sum(b[,1]>b[,2] & b[,2]>b[,3])/iter
+    a$SDs[p]<-paste(round(diag(sigma_beta), digits=3), collapse="; ")
 }
 a
 #Conclusion: 
@@ -49,4 +73,4 @@ a
 
 
 #######################################################################
-0.3^2
+
