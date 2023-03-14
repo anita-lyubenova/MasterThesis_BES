9e83f57d028df1020b6ef6339e9bbf08cb277268
anita-lyubenova <anitaencheva@gmail.com>
2023-03-14 16:06:22 +0100

    single_sim(): add heterogeneity; add more outputs
    betas can be sampled from mvnorm

--- Scripts/single_sim().R
+++ Scripts/single_sim().R
@@ -16,38 +16,68 @@ source("scripts/ThomVolker scripts/functions.R")
 # iter: number of iterations
 single_sim<-function(r2,  # R-squared of the regression model
                      pcor,  # correlation between the predictors
-                     betas,  # a numeric vector with the beta coefficients;  defines the truth in the population;
-                     Sigma_beta,  # variance covariance matrix of the (true) regression parameters - can be used to induce heterogeneity
+                     betas,  # a numeric vector with the true beta parameters (or the means of the mvnorm dist, if they are sampled);
+                    # Sigma_beta,  # variance covariance matrix of the (true) regression parameters - can be used to induce heterogeneity
+                     propSD=0, #the proportion of the effect size used to determine the SD of the parameters: SD=propSD*betas, 70% is a wide distribution (REF)
                      hypothesis,  # the hypothesis of interest; must be in the format of bain()
                      n,  #sample size 
                      model,  #linear, logistic or probit regression
-                     save_mod_coefs # should the estimated coefficients and their standard errors be saved?
+                     save_mod_coefs=TRUE # should the estimated coefficients and their standard errors be saved?
 ){
   #n.hyp<-length(unlist(strsplit(hypothesis, ";")))
-  n.hyp<-1
-
+  n.hyp<-1 # for now I'd limit the user to only testing a single hypothesis
+  
+  input_betas<-betas
+  Sigma_beta<-diag(x=(propSD*input_betas)^2, nrow = length(input_betas),ncol = length(input_betas))
+  
+  #sample true betas
+  betas<-mvrnorm(n=1, mu=input_betas, Sigma = Sigma_beta)
+  
   #obtain BFiu
-  BF<-gen_dat(r2=r2,
+  lm.mod<-gen_dat(r2=r2,
                betas=betas,
                rho=cormat(pcor, length(betas)),
                n=n,
                model="normal") %>% 
-     lm(Y~., data=.) %>%
+     lm(Y~., data=.)
+  
+  est_betas<-coef(lm.mod)[-1]
+  est_SE<-summary(lm.mod)$coefficients[-1,2]
+  
+  BF<-lm.mod %>% 
      bain(hypothesis = hypothesis)%$%fit %>%     #$BF.u[c(1,2,4)]
-     extract(c(1:n.hyp, nrow(.)),"BF.u") #subset only BFiu for the specified hypothesis and the complement
+     extract(c(1:n.hyp, nrow(.)),"BF.u") %>%  #subset only BFiu for the specified hypothesis and the complement
+     c(.,1)#add BFuu = 1
    
+  list(BF=BF,
+       inputs=list(r2=r2,
+                   pcor=pcor,
+                   betas=input_betas,
+                   propSD=propSD,
+                   hypothesis=hypothesis,
+                   n=n,
+                   model=model
+                    ),
+       sampled_betas=betas,
+       Sigma_beta=Sigma_beta,
+       est_betas=est_betas,
+       est_SE=est_SE
+       )
 }
+betas<-coefs(0.13, c(3,2,1), cormat(0.3, 3), "normal")
+sigma_beta<-diag(x=(1*betas)^2, nrow = length(betas),ncol = length(betas))
 
 a<-single_sim(r2=0.13,  # R-squared of the regression model
               pcor=0.3,  # correlation between the predictors
               betas=coefs(0.13, c(3,2,1), cormat(0.3, 3), "normal"),  # a numeric vector with the beta coefficients;  defines the truth in the population;
-              Sigma_beta=NULL,  # variance covariance matrix of the (true) regression parameters - can be used to induce heterogeneity
+             # Sigma_beta=NULL,  # variance covariance matrix of the (true) regression parameters - can be used to induce heterogeneity
+              propSD = 0.2,
               hypothesis="V1>V2>V3",  # the hypothesis of interest; must be in the format of bain()
               n=100,  #sample size 
               model="linear",  #linear, logistic or probit regression
               save_mod_coefs=NULL # should the estimated coefficients and their standard errors be saved?
 )
-
+a
 #Parallelization --------------------------------------------------------------
 
 library(foreach)
@@ -84,7 +114,6 @@ results <- foreach (i=1:5000,
 
 ### parallel iterations x n----------------------------------------------------------
 library(abind)
-
 
 
 abind_3<-function(...){
@@ -168,7 +197,7 @@ betas<-coefs(r2, ratio_beta, cormat(pcor, length(ratio_beta)), "normal")
 # sigma_beta<-diag(x=0.10*betas, nrow = length(betas),ncol = length(betas)) #covariance matrix of the coefficients
 
 #How often is b1>b2>b3 for SD being the specified percentage of the coefficients
-percentage=c(seq(0.01, 0.09, by=0.01),seq(0.1, 0.9, by=0.1))
+percentage=c(seq(0.01, 0.09, by=0.01),seq(0.1, 0.9, by=0.1), 0.95, 1)
 iter=10000
 a<-data.frame(percentage=percentage,
               prop_correct=NA,
@@ -184,13 +213,6 @@ for(p in 1:length(percentage)){
     a$SDs[p]<-paste(round(diag(sigma_beta), digits=3), collapse="; ")
 }
 a
-#Conclusion: 
-#(1) If the true coefficients are sampled (instead of fixed betas), even with very small 
-#    SDs of the MVnorm, the sampled coefficients will not be in line with the informative hypothesis 10-20-30-40% of the time
-#    ==> It will be hard/not possible to have heterogeneity of the effects and have H1 to be true for all studies...
-
-# (2) The more parsimonious the hypothesis, the more prone to rejection it is in case there is heterogeneity
-
-
-#######################################################################
+# Insights:
+# - The more parsimonious the hypothesis, the more prone to rejection it is in case there is heterogeneity
 
