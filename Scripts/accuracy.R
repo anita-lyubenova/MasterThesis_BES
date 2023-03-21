82945f7adf150450c56eed082b2223d09614d57b
anita-lyubenova <anitaencheva@gmail.com>
2023-03-21 08:21:57 +0100

    remove manual accuracy plots

--- Scripts/accuracy.R
+++ Scripts/accuracy.R
@@ -5,57 +5,6 @@
 
 load("Outputs/accuracy/dat_merged.RData")
 source("Scripts/accuracy/accuracy_functions.R")
-# H1 vs Hu
-a<-aggregatePMP(dat,
-                hyp=c("H1","Hu"),
-                studies = 10)
-
-a.acc<-accuracyPMP(listPMP = a,
-            hyp_to_pop =c(H1="TRUE_H1", Hu="TRUE_Hc", Hu="TRUE_Hu")
-            )
-
-# VS.
-
-#H1 vs. Hc vs. Hu
-b<-aggregatePMP(dat,
-                hyp=c("H1","Hc","Hu"),
-                studies = 10)
-
-b.acc<-accuracyPMP(listPMP = b,
-            hyp_to_pop =c(H1="TRUE_H1", Hc="TRUE_Hc", Hu="TRUE_Hu")
-)
-a.acc$acc
-b.acc$acc
-
-
-
-a.plot<-
-  ggcorrplot(a.acc$acc,
-             outline.col = "white",
-             lab = TRUE)+
-  scale_fill_gradient2(limit = c(0,1),
-                       low = "blue", high =  "red",
-                       mid = "white",
-                       midpoint = 0.87)+
-  scale_x_continuous(breaks = 1:a.acc$studies)+
-  labs(title = a.acc$hypothesis_test,
-       subtitle =paste("Populations:", paste(a.acc$hyp_to_pop, collapse = ", "), collapse = " ")
-       )
-a.plot
-b.plot<-ggcorrplot(b.acc$acc,
-                   outline.col = "white",
-                   lab = TRUE)+
-  scale_fill_gradient2(limit = c(0,1),
-                       low = "blue", high =  "red",
-                       mid = "white",
-                       midpoint = 0.87)+
-  scale_x_continuous(breaks = 1:b.acc$studies)+
-  labs(title = b.acc$hypothesis_test,
-       subtitle =paste("Populations:", paste(b.acc$hyp_to_pop, collapse = ", "), collapse = " ")
-       )
-b.plot
-
-a.plot/b.plot
 
 
 
