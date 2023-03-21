22acc406a9d9536b06118673da129f7df31c1b1e
anita-lyubenova <anitaencheva@gmail.com>
2023-03-21 08:20:17 +0100

    move accuracy functions to a separate script

--- Scripts/accuracy.R
+++ Scripts/accuracy.R
@@ -3,98 +3,8 @@
 # [compl_ inluded additional populations with heterogeneity due to sampled betas with varying SDs]
 # after merging them into BFdat
 
-library(tidyverse)
-library(abind)
-load("Outputs/accuracy/dat (merged simulated files).RData")
-
-#a function to transform BFs to aggregated PMPs
-aggregatePMP<-function(x, # a 5 dim array with structure [t, BF, pop, iter, n]
-                       hyp=c("H1", "Hu"),
-                       studies=40 #number of studies to aggregate over, max 40
-){
-  hyp_index<-substr(hyp,2,2)
-  BF<-x$BF
-  BF<-BF[1:studies,substr(dimnames(BF)[[2]],3,3) %in% hyp_index,,,,drop=FALSE]
-  
-  nom<-aperm(BF, perm=c(1,3,4,5,2))
-  denom<-rowSums(nom, dims = 4)
-  PMP_perm<-nom/replicate(length(hyp),denom)
-  
-  #placeholder for tha aggregated PMPs
-  PMP_t<-PMP_perm
-  for(t in 2:studies){ #the PMPs of the first study remain the same, thus iterate from t=2
-    nom_t<-PMP_t[t-1,,,,]*nom[t,,,,]#multiply the previous PMPs with the current BFs
-    denom_t<-rowSums(nom_t, dims=3)
-    PMP_t[t,,,,]<-nom_t/replicate(length(hyp), denom_t)
-  }
-  
-  PMP_t<-aperm(PMP_t, perm = c(1,5,2,3,4))
-  dimnames(PMP_t)[[2]]<-paste0("PMP", c(hyp_index))
-  PMP_t<-list(PMP=PMP_t,
-              r2=x$r2,
-              pcor=x$pcor,
-              populations=x$populations,
-              hypothesis=x$hypothesis,
-              model=x$model,
-              iter=x$iter,
-              studies=studies,
-              hypothesis_test = paste(hyp, collapse = " vs. ")
-              )
-  
-  return(PMP_t)
-}
-a<-aggregatePMP(dat,
-                hyp=c("H1","Hc","Hu"),
-                studies = 10)
-
-
-#a function to compute confusion matrix from aggregated PMPs
-accuracyPMP<-function(listPMP, #list created with aggregatePMP()
-                     hyp_to_pop # a named character vector; elements are subset of dimnames(PMP)[[3]]: "TRUE_H0", "TRUE_H1", "TRUE_Hc", "TRUE_Hu", "HETEROG_H1p.1", "HETEROG_H1p.3", "HETEROG_H1p.5", names are the hypotheses for which the populations are true
-                    
-){
-  #subset the populations of interest
-  PMP<-listPMP$PMP[,,hyp_to_pop,,]
-  dim(PMP)
-  hyp_index<-substr(names(hyp_to_pop),2,2)
-  
-  tr<-data.frame(hyp=names(hyp_to_pop),
-                 PMP=paste0("PMP", hyp_index),
-                 pop=hyp_to_pop
-                 )
-  
-  correct_name<-paste0("c", tr$pop)
-
-  for(h in 1:nrow(tr)){
-    
-    comp<- paste(paste0("PMP[,tr$PMP[h],tr$pop[h],,,drop=FALSE] > ",
-                        paste0("PMP[,",which(!dimnames(PMP)[[2]] %in% tr$PMP[h]), ",tr$pop[h],,,drop=FALSE]")
-                        ),collapse = " & ")
-    
-    correct_logical<-eval(parse(text = comp))
-
-    correct_count<-rowSums(aperm(correct_logical, c(1,2,3,5,4)), dims = 4)[,,,,drop=TRUE]
-
-    assign(correct_name[h], correct_count)
-  }
-  
-  
-  acc<-eval(parse(text = paste(correct_name, collapse = "+")))/(nrow(tr)*listPMP$iter)
-  
-  list(acc=round(acc, digits = 3),
-       r2=listPMP$r2,
-       pcor=listPMP$pcor,
-       hypothesis=listPMP$hypothesis,
-       model=listPMP$model,
-       iter=listPMP$iter,
-       studies=listPMP$studies,
-       populations=listPMP$populations[hyp_to_pop],
-       hypothesis_test=listPMP$hypothesis_test,
-       hyp_to_pop=hyp_to_pop
-       )
-
-}
-
+load("Outputs/accuracy/dat_merged.RData")
+source("Scripts/accuracy/accuracy_functions.R")
 # H1 vs Hu
 a<-aggregatePMP(dat,
                 hyp=c("H1","Hu"),
@@ -117,8 +27,7 @@ b.acc<-accuracyPMP(listPMP = b,
 a.acc$acc
 b.acc$acc
 
-library(ggcorrplot)
-library(patchwork)
+
 
 a.plot<-
   ggcorrplot(a.acc$acc,
@@ -149,20 +58,6 @@ b.plot
 a.plot/b.plot
 
 
-acc_corrplot<-function(a # list created with accuracyPMP()
-                       ){
-  ggcorrplot(a$acc,
-             outline.col = "white",
-             lab = TRUE)+
-    scale_fill_gradient2(limit = c(0,1),
-                         low = "blue", high =  "red",
-                         mid = "white",
-                         midpoint = 0.87)+
-    scale_x_continuous(breaks = 1:a$studies)+
-    labs(title = a$hypothesis_test,
-         subtitle =paste("Populations:", paste(a$hyp_to_pop, collapse = ", "), collapse = " ")
-    )
-}
 
 #
 aggregatePMP(x=dat,
