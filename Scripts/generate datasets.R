f9c0e92b3af2b21239ae703168145e9ac06d2d84
anita-lyubenova <anitaencheva@gmail.com>
2023-03-27 22:14:02 +0200

    script to generate datasets

---
+++ Scripts/generate datasets.R
@@ -0,0 +1,99 @@
+
+library(foreach)
+library(doParallel)
+library(abind)
+
+#source("scripts/ThomVolker scripts/functions.R")
+source("scripts/generate_datasets().R")
+
+#Conditions:
+n = c(25,50,100,150,200,250,300,350,500, 800)
+iterations = 1:40000 # 1000 iter x 40 studies#
+p = c(0.1, 0.3, 0.5, 0.70, 1)
+
+
+#Generate TRUE_H1_medium---------------------------------------------------------------------------
+#parallel t x heterog x iter x n
+
+#resulting data:
+# list[[n]] $BF[t, BF, heterog, iter]
+#           $sampled_betas[t, beta_index, heterog, iter] #here are the true betas (that were sampled in each iteration)
+#           $est_betas[t, beta_index, heterog, iter] #betas estimated from lm()
+#           $est_SE[t, beta_index, heterog, iter] #standard errors of the estimated betas from lm()
+
+
+
+#registerDoParallel(7)  # use multicore, set to the number of cores
+
+
+library(doSNOW)
+library(doRNG)
+
+cl <- makeCluster(7)
+registerDoSNOW(cl)
+
+# #setting seed
+n <-  c(25,100,200,250,350,500, 800)
+iter<-20000
+rng <- RNGseq(length(n)* iter, 123)
+
+#add progress bar
+iterations <- length(n)*iter # total number of conditions - not sure if this actually matters
+pb <- txtProgressBar(max = iterations, style = 3)
+progress <- function(x) setTxtProgressBar(pb, x)
+opts <- list(progress = progress)
+#end progress bar options
+
+# run standard nested foreach loo
+## foreach loops
+system.time(
+  
+  TRUE_H1 <- 
+    
+    #n-loop: sample size
+    foreach(s = 1:length(n), #,
+       #     .combine=list,
+            .options.snow = opts, #add progress bar
+            .packages = c("tidyverse","magrittr", "furrr",
+                          "Rcpp", "RcppArmadillo", "MASS",
+                          "mvtnorm", "bain", "foreach", "doParallel")
+    ) %:% #{
+    
+    #i-loop: iterations
+    foreach(i = 1:iter, #iterations
+            r=rng[(s-1)*iter + 1:iter],
+            #.combine = list, #bind along the 4th dimension
+            .packages = c("tidyverse","magrittr", "furrr",
+                          "Rcpp", "RcppArmadillo", "MASS",
+                          "mvtnorm", "bain", "foreach", "doParallel")
+    )%dopar% {
+      
+      # set RNG seed
+      rngtools::setRNG(r)
+      
+      generate_dataset(r2=0.13,  # R-squared of the regression model
+                            pcor=0.3,  # correlation between the predictors
+                            betas=coefs(0.13, c(3,2,1), cormat(0.3, 3), "normal"),  # a numeric vector with the beta coefficients;  defines the truth in the population;
+                            propSD = 0,
+                            n=n[s],  #sample size 
+                            model="linear"  #linear, logistic or probit regression
+      )$d
+      
+      # #save the results in a list
+      # res<-list(d=sim$BF,
+      #           sampled_betas=sim$sampled_betas
+      # )
+      
+    } #end foreach t loop (studies) (rows)
+
+  
+)#system.time
+#time: 7cl - about 1h 15 min
+close(pb)
+stopCluster(cl) 
+
+
+TRUE_H1[[1]][[1]][1,] == TRUE_H1_repr[[1]][[1]][1,]
+#Generate TRUE_Hc---------------------------------------------------------------------------
+
+#Generate TRUE_H0---------------------------------------------------------------------------
--- Scripts/generate_datasets().R
+++ Scripts/generate_datasets().R
@@ -23,6 +23,7 @@ generate_dataset<-function(r2,  # R-squared of the regression model
              n=n,
              model="normal")
   
+  
   list(d=d,
        sampled_betas=betas)
   # list(d=d,
@@ -38,16 +39,16 @@ generate_dataset<-function(r2,  # R-squared of the regression model
   # )
 }
 
-a<-generate_dataset(r2=0.13,  # R-squared of the regression model
-                    pcor=0.3,  # correlation between the predictors
-                    betas=coefs(0.13, c(3,2,1), cormat(0.3, 3), "normal"),  # a numeric vector with the beta coefficients;  defines the truth in the population;
-                    # Sigma_beta=NULL,  # variance covariance matrix of the (true) regression parameters - can be used to induce heterogeneity
-                    propSD = 0.2,
-                    #  hypothesis="V1>V2>V3",  # the hypothesis of interest; must be in the format of bain()
-                    n=100,  #sample size 
-                    model="linear"  #linear, logistic or probit regression
-                    # save_mod_coefs=NULL # should the estimated coefficients and their standard errors be saved?
-)
-a
+# a<-generate_dataset(r2=0.13,  # R-squared of the regression model
+#                     pcor=0.3,  # correlation between the predictors
+#                     betas=coefs(0.13, c(3,2,1), cormat(0.3, 3), "normal"),  # a numeric vector with the beta coefficients;  defines the truth in the population;
+#                     # Sigma_beta=NULL,  # variance covariance matrix of the (true) regression parameters - can be used to induce heterogeneity
+#                     propSD = 0.2,
+#                     #  hypothesis="V1>V2>V3",  # the hypothesis of interest; must be in the format of bain()
+#                     n=100,  #sample size
+#                     model="linear"  #linear, logistic or probit regression
+#                     # save_mod_coefs=NULL # should the estimated coefficients and their standard errors be saved?
+# )
+# a
 
 
