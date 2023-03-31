c0f28917b0216e962ad059cc922f8a6db1516e61
anita-lyubenova <anitaencheva@gmail.com>
2023-03-31 10:38:22 +0200

    Update generate datasets.R
    generate TRUE_Hc and TRUE_H0

--- Scripts/generate datasets.R
+++ Scripts/generate datasets.R
@@ -6,10 +6,10 @@ library(abind)
 #source("scripts/ThomVolker scripts/functions.R")
 source("scripts/generate_datasets().R")
 
-#Conditions:
-n = c(25,50,100,150,200,250,300,350,500, 800)
-iterations = 1:40000 # 1000 iter x 40 studies#
-p = c(0.1, 0.3, 0.5, 0.70, 1)
+# #Conditions:
+# n = c(25,50,100,150,200,250,300,350,500, 800)
+# iterations = 1:40000 # 1000 iter x 40 studies#
+# p = c(0.1, 0.3, 0.5, 0.70, 1)
 
 
 #Generate TRUE_H1_medium---------------------------------------------------------------------------
@@ -34,7 +34,7 @@ registerDoSNOW(cl)
 
 # #setting seed
 n <-  c(25,100,200,250,350,500, 800)
-iter<-20000
+iter<-40000
 rng <- RNGseq(length(n)* iter, 123)
 
 #add progress bar
@@ -92,8 +92,140 @@ system.time(
 close(pb)
 stopCluster(cl) 
 
-
+save(TRUE_H1,file="Outputs/generate datasets/TRUE_H1.RData")
 TRUE_H1[[1]][[1]][1,] == TRUE_H1_repr[[1]][[1]][1,]
 #Generate TRUE_Hc---------------------------------------------------------------------------
 
+cl <- makeCluster(7)
+registerDoSNOW(cl)
+
+# #setting seed
+n <-  c(25,100,200,250,350,500, 800)
+iter<-40000
+rng <- RNGseq(length(n)* iter, 123)
+
+#add progress bar
+iterations <- length(n)*iter # total number of conditions - not sure if this actually matters
+pb <- txtProgressBar(max = iterations, style = 3)
+progress <- function(x) setTxtProgressBar(pb, x)
+opts <- list(progress = progress)
+#end progress bar options
+
+# run standard nested foreach loo
+## foreach loops
+system.time(
+  
+  TRUE_Hc <- 
+    
+    #n-loop: sample size
+    foreach(s = 1:length(n), #,
+            #     .combine=list,
+            .options.snow = opts, #add progress bar
+            .packages = c("tidyverse","magrittr", "furrr",
+                          "Rcpp", "RcppArmadillo", "MASS",
+                          "mvtnorm", "bain", "foreach", "doParallel")
+    ) %:% #{
+    
+    #i-loop: iterations
+    foreach(i = 1:iter, #iterations
+            r=rng[(s-1)*iter + 1:iter],
+            #.combine = list, #bind along the 4th dimension
+            .packages = c("tidyverse","magrittr", "furrr",
+                          "Rcpp", "RcppArmadillo", "MASS",
+                          "mvtnorm", "bain", "foreach", "doParallel")
+    )%dopar% {
+      
+      # set RNG seed
+      rngtools::setRNG(r)
+      
+      generate_dataset(r2=0.13,  # R-squared of the regression model
+                       pcor=0.3,  # correlation between the predictors
+                       betas=coefs(0.13, c(1,2,3), cormat(0.3, 3), "normal"),  # a numeric vector with the beta coefficients;  defines the truth in the population;
+                       propSD = 0,
+                       n=n[s],  #sample size 
+                       model="linear"  #linear, logistic or probit regression
+      )$d
+      
+      # #save the results in a list
+      # res<-list(d=sim$BF,
+      #           sampled_betas=sim$sampled_betas
+      # )
+      
+    } #end foreach t loop (studies) (rows)
+  
+  
+)#system.time
+#time: 7cl - about 1h 15 min
+close(pb)
+stopCluster(cl) 
+
+save(TRUE_Hc,file="Outputs/generate datasets/TRUE_Hc.RData")
+
 #Generate TRUE_H0---------------------------------------------------------------------------
+
+cl <- makeCluster(7)
+registerDoSNOW(cl)
+
+# #setting seed
+n <-  c(25,100,200,250,350,500, 800)
+iter<-40000
+rng <- RNGseq(length(n)* iter, 123)
+
+betas<-coefs(0.13, c(1,1,1), cormat(0.3, 3), "normal")
+
+#add progress bar
+iterations <- length(n)*iter # total number of conditions - not sure if this actually matters
+pb <- txtProgressBar(max = iterations, style = 3)
+progress <- function(x) setTxtProgressBar(pb, x)
+opts <- list(progress = progress)
+#end progress bar options
+
+# run standard nested foreach loo
+## foreach loops
+system.time(
+  
+  TRUE_H0 <- 
+    
+    #n-loop: sample size
+    foreach(s = 1:length(n), #,
+            #     .combine=list,
+            .options.snow = opts, #add progress bar
+            .packages = c("tidyverse","magrittr", "furrr",
+                          "Rcpp", "RcppArmadillo", "MASS",
+                          "mvtnorm", "bain", "foreach", "doParallel")
+    ) %:% #{
+    
+    #i-loop: iterations
+    foreach(i = 1:iter, #iterations
+            r=rng[(s-1)*iter + 1:iter],
+            #.combine = list, #bind along the 4th dimension
+            .packages = c("tidyverse","magrittr", "furrr",
+                          "Rcpp", "RcppArmadillo", "MASS",
+                          "mvtnorm", "bain", "foreach", "doParallel")
+    )%dopar% {
+      
+      # set RNG seed
+      rngtools::setRNG(r)
+      
+      generate_dataset(r2=0.13,  # R-squared of the regression model
+                       pcor=0.3,  # correlation between the predictors
+                       betas=betas,  # a numeric vector with the beta coefficients;  defines the truth in the population;
+                       propSD = 0,
+                       n=n[s],  #sample size 
+                       model="linear"  #linear, logistic or probit regression
+      )$d
+      
+      # #save the results in a list
+      # res<-list(d=sim$BF,
+      #           sampled_betas=sim$sampled_betas
+      # )
+      
+    } #end foreach t loop (studies) (rows)
+  
+  
+)#system.time
+#time: 7cl - about 1h 15 min
+close(pb)
+stopCluster(cl) vr                             
+
+save(TRUE_H0,file="Outputs/generate datasets/TRUE_H0.RData")
