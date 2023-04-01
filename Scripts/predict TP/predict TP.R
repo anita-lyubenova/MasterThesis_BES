c823d2d0fd166bc34b70e35e06854a1fb516c4db
anita-lyubenova <anitaencheva@gmail.com>
2023-04-01 23:39:35 +0200

    initial workflow to compute TPR

--- Scripts/predict TP/predict TP.R
+++ Scripts/predict TP/predict TP.R
@@ -6,6 +6,170 @@
 library(tidyverse)
 library(bain)
 library(magrittr)
+library(parallel)
+library(foreach)
+library(doParallel)
+# test.RData ---------------------------------------------------------------
+
+load("Outputs/generate datasets/test.RData")
+iter<-10
+
+#function to compute BFiu, BFcu, adn BFuu for each dataset
+get_BF<-function(x){
+  lm(Y~., data=x)%>% 
+    bain(hypothesis = "V1>V2>V3")%$%fit %>%     #$BF.u[c(1,2,4)]
+    extract(c(1, nrow(.)),c("BF.u")) %>%  #subset only BFiu for the specified hypothesis and the complement
+    c(.,"BFuu"=1)
+}
+
+## 1. replace each dataset with BFiu, cu and uu --------------------------------
+cl <- makeCluster(7)
+#clusterExport(cl, "test")
+clusterEvalQ(cl, {
+  library(bain)
+  library(magrittr)
+})
+
+#compute BFiu, BFcu adn BFuu for each dataset
+system.time(
+  BF_list <- 
+    lapply(test, function(x){
+      parLapply(cl, x, get_BF)
+        }
+      )
+)
+
+stopCluster(cl)
+
+#non-parallel vesrion
+BF_list <-
+  lapply(test, function(x){
+    lapply(x, get_BF)
+  }
+)
+
+
+## 2. rbind the BFs for a study set ------------------------------------------
+cl <- makeCluster(4)
+clusterEvalQ(cl, {
+  library(tidyverse)
+})
+BF_unl<-
+  parLapply(cl, BF_list, function(x){
+    d<-do.call(rbind, x)
+    d <- data.frame(iter = rownames(d), d, row.names = NULL)
+    colnames(d)<-c("iter", "BF1u", "BFcu", "BFuu")
+    d<-split(d, d$iter)
+   # return(d)
+  } )
+
+stopCluster(cl)
+
+
+## 2.5 abind to array ----------------------------------------------------------
+library(abind)
+BF_unl$n100$i9[,-1]
+
+BF_3darray<-lapply(BF_unl, function(x){
+  do.call(abind, args=list(x, along=3) )
+} )
+BF_4d<-do.call(abind, args=list(BF_3darray, along=4))
+BF_4d<-BF_4d[,-1,,]#remove column iter
+
+class(BF_4d) <- "numeric" #transform BFs from character to numeric
+
+# array structure :: BF_4d[t, hyp, iter, n]
+
+## 3. compute aggregate. PMPs -----------------------------------------------------
+x<-BF_4d
+#a function to transform BFs to aggregated PMPs
+aggregatePMP<-function(x, # a 4 dim array with structure [t, BF, pop, iter, n]
+                       hyp=c("H1", "Hu"),
+                       studies=10 #number of studies to aggregate over, max 40
+){
+  hyp_index<-substr(hyp,2,2)
+  BF<-x
+  BF<-BF[1:studies,substr(dimnames(BF)[[2]],3,3) %in% hyp_index,,,drop=FALSE]
+  
+  nom<-aperm(BF, perm=c(1,3,4,2))
+  denom<-rowSums(nom, dims = 3)
+  PMP_perm<-nom/replicate(length(hyp),denom)
+  
+  #placeholder for tha aggregated PMPs
+  PMP_t<-PMP_perm
+  for(t in 2:studies){ #the PMPs of the first study remain the same, thus iterate from t=2
+    nom_t<-PMP_t[t-1,,,]*nom[t,,,]#multiply the previous PMPs with the current BFs
+    denom_t<-rowSums(nom_t, dims=2)
+    PMP_t[t,,,]<-nom_t/replicate(length(hyp), denom_t)
+  }
+  
+  PMP_t<-aperm(PMP_t, perm = c(1,4,2,3))
+  dimnames(PMP_t)[[2]]<-paste0("PMP", c(hyp_index))
+  dimnames(PMP_t)[[1]]<-1:studies
+  
+  return(PMP_t)
+}
+
+
+
+PMP<-aggregatePMP(BF_4d, c("H1","Hu"), studies = 3)
+
+# 4. compute the TP rates
+
+
+#a function to compute confusion matrix from aggregated PMPs
+TPRi<-function(PMP, #an array created with aggregatePMP()[studies, hyp, iter, n]
+               true_pop = "H1" # the true population
+                      
+){
+  #subset the populations of interest
+  #PMP<-listPMP$PMP[,,hyp_to_pop,,]
+  dim(PMP)
+  hyp_index<-substr(true_pop,2,2)
+  true_pop_col<-paste0("PMP", hyp_index)
+  
+  correct_logical<-PMP[,true_pop_col,,]>PMP[,!dimnames(PMP)[[2]] %in% true_pop_col,,]
+  #compute the number of correct classifications by summing the TRUE values
+  correct_count<-rowSums(aperm(correct_logical, c(1,3,2)),# iterations -> last dim
+                         dims = 2#sum over the last dimension (iterations)
+                         )
+  TPR<-correct_count/dim(PMP)[[3]]
+  
+  TPR<-
+    TPR %>% 
+    as.data.frame() %>% 
+    rownames_to_column(var="t") %>% 
+    pivot_longer(cols =starts_with("n"),
+                 names_to = "n",
+                 values_to = "TPR") %>% 
+    mutate(n=as.numeric(substr(n,2,nchar(n))),
+           t=as.numeric(t))
+  return(TPR)
+  
+}
+
+
+
+
+aggregatePMP(BF_4d, c("H1","Hu"), studies = 4) %>% 
+  TPRi(true_pop = "H1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#TRUE_H1.RData --------------------------------------------------------
 load("Outputs/generate datasets/TRUE_H1.RData")
 n<-c(25,100,200,250,350,500, 800)
 iter<-1000
