e774394c89154bcdc97d8c004ce047a5a685a51e
anita-lyubenova <anitaencheva@gmail.com>
2023-04-27 10:36:48 +0200

    scripts for hpc

---
+++ scripts for hpc/pre-process.R
@@ -0,0 +1,338 @@
+# This scripts combines the simulation files created with the simulate.R script
+# Version 1: every population was saved in  a different rds file
+# Version 2: populations were saved in one list
+# ################################################################################################## 2
+# #                                    ---- Version 1 ----
+# ################################################################################################## 2
+# 
+# library(dplyr)
+# library(abind)
+# 
+# source("Part I/pre-processing/pre-processing functions.R")
+# 
+# #Load files 
+# filenames<-list.files("Part I/data simulation/output", pattern = ".rds")
+# path<-"Part I/data simulation/output"
+# 
+# varnames<-paste0("BF_pop_", 1:length(filenames))
+# 
+# #load each simulation file (i.e., population), to an object with name specified in the varnames vector
+# # the resulting BF_pop_1,BF_pop_2, etc. are lists, where elements correspond to different n,
+# # and contain data.frames where [iterations*studies, BF_hyp]
+# for(i in 1:length(filenames)){
+#   assign(varnames[i],readRDS(paste0(path,"/", filenames[i])))
+# }
+# 
+# #Simulation conditions
+# # I use only the attributes from one of the populations, because they are the same across populations
+# n = attributes(BF_pop_1)$n
+# studies<-attributes(BF_pop_1)$studies
+# iterations=attributes(BF_pop_1)$iterations
+# hypothesis<-attributes(BF_pop_1)$hypothesis
+# 
+# #a vector with population names obtained from the name of the files
+# pop_names<-sub("\\.rds.*", "", filenames)  
+# 
+# #reshape the lists to 4d arrays where [studies, BF_hyp, iter, n],  i.e.
+# #   -studies*iterations are split to different dimensions
+# #   -levels of n are put into a dimension
+# for(i in 1:length(pop_names)){
+#   assign(pop_names[i],  reshapeBFs(BF_list=eval(parse(text=varnames[i])), # a list, where each element (a dataframe, col=hypotheses, rows=iterations*studies) contains the BFs for a certain sample size n
+#                                    n=n,
+#                                    studies=studies,
+#                                    iterations=iterations)
+#          )
+# }
+# 
+# #save attributes of the resulting arrays 
+# #again, they are the same across populations
+# att<-attributes(eval(parse(text=pop_names[1])))
+# #remove the attributes about dimensionality, and keep only the attributes relevant to the simulation conditions
+# att<-att[names(att)[-grep("dim", names(att))]]
+# 
+# #bind the 4d arrays (i.e., populations) along the 5th dimension of the array
+# BF_bind <-do.call(abind, args=list(mget(pop_names), along=5)) %>% 
+#   #and reorder the dims such that the structure is [t,BF_hyp, pop, iter,n]
+#   aperm(perm=c(1,2,5,3,4)) 
+# 
+# 
+# #add BFuu = 1
+# #First, create an array slice for BFuu
+# Hu_array<-array(1, dim = c(studies,
+#                            1,
+#                            length(filenames),
+#                            iterations,
+#                            length(n)
+#                            ))
+# #combine the array slicefor BFuu with the main array
+# BF_bind2<-abind(BF_bind, Hu_array, along = 2)
+# #give the BFuu slice a name "Hu"
+# dimnames(BF_bind2)[[2]]<-c("H1", "Hc", "Hu")
+# 
+# 
+# attributes(BF_bind2)<-c(attributes(BF_bind2),
+#                        att)
+# 
+# #save the complete 5d array
+# saveRDS(BF_bind2, "Part I/pre-processing/output/BF_data.rds")
+# 
+
+
+################################################################################################## 2
+#                                    ---- Version 2 ----
+################################################################################################## 2
+
+library(dplyr)
+library(abind)
+
+source("~/pre-processing/pre-processing functions.R")
+
+res3par<-readRDS(file= "~/simulation/output/res3par.rds")
+
+
+#n_par 3 ----------------------------
+#Simulation conditions
+# I use only the attributes from one of the populations, because they are the same across populations
+n = attributes(res3par[[1]])$n
+studies<-attributes(res3par[[1]])$studies
+iterations=attributes(res3par[[1]])$iterations
+hypothesis<-attributes(res3par[[1]])$hypothesis
+#all populations
+pop_names<-sapply(res3par, function(x){
+  attributes(x)$pop_name
+} )
+r2<-sapply(res3par, function(x){
+  attributes(x)$r2
+} ) %>% unique()
+pcor<-sapply(res3par, function(x){
+  attributes(x)$pcor
+} ) %>% unique()
+p<-sapply(res3par, function(x){
+  attributes(x)$p
+} ) %>% unique()
+ratio_beta<-lapply(res3par, function(x){
+  attributes(x)$ratio_beta
+} ) %>% unique()
+
+att<-list(n=n,
+          studies=studies,
+          iterations=iterations,
+          hypothesis=hypothesis,
+          pop_names=pop_names,
+          r2=r2,
+          pcor=pcor,
+          p=p,
+          ratio_beta=ratio_beta
+          )
+#reshape the inner lists to 4d arrays where [studies, BF_hyp, iter, n],  i.e.
+#   -studies*iterations are split to different dimensions
+#   -levels of n are put into a dimension
+# => array4d is a list condtaining 4d arrays of lenght the number of populations
+array4d<-lapply(res3par, function(x){
+  reshapeBFs(BF_list=x, # a list, where each element (a dataframe, col=hypotheses, rows=iterations*studies) contains the BFs for a certain sample size n
+             n=n,
+             studies=studies,
+             iterations=iterations)
+})
+
+# #save attributes of the resulting arrays that are to be used for the final data
+# #again, they are the same across populations
+# att<-attributes(array4d[[1]])
+# #remove the attributes about dimensionality, and keep only the attributes relevant to the simulation conditions
+# att<-att[names(att)[-grep("dim", names(att))]]
+# #remove the "names" attribute
+# att<-att[names(att)[-grep("names", names(att))]]
+
+
+#bind the 4d arrays (i.e., populations) along the 5th dim
+BF_bind<-do.call(what = "abind", args=list(array4d, along=5))%>% 
+  #and reorder the dims such that the structure is [t,BF_hyp, pop, iter,n]
+  aperm(perm=c(1,2,5,3,4)) 
+
+#name the populations contained in teh 3th dim
+dimnames(BF_bind)[[3]]<-pop_names
+
+#add BFuu = 1
+#First, create an array slice for BFuu
+Hu_array<-array(1, dim = c(studies,
+                           1,
+                           length(pop_names),
+                           iterations,
+                           length(n)
+))
+#combine the array slicefor BFuu with the main array
+BF_bind3<-abind(BF_bind, Hu_array, along = 2)
+#give the BFuu slice a name "Hu"
+dimnames(BF_bind3)[[2]][length(dimnames(BF_bind3)[[2]])]<-"Hu"
+
+attributes(BF_bind3)<-c(attributes(BF_bind3),
+                        att)
+saveRDS(BF_bind3, file="~/pre-processing/output/BF_data_3par.rds")
+# n_par 2 ---------------------------------------------
+
+res2par<-readRDS(file= "~/simulation/output/res2par.rds")
+
+#Simulation conditions
+# I use only the attributes from one of the populations, because they are the same across populations
+n = attributes(res2par[[1]])$n
+studies<-attributes(res2par[[1]])$studies
+iterations=attributes(res2par[[1]])$iterations
+hypothesis<-attributes(res2par[[1]])$hypothesis
+#all populations
+pop_names<-sapply(res2par, function(x){
+  attributes(x)$pop_name
+} )
+r2<-sapply(res2par, function(x){
+  attributes(x)$r2
+} ) %>% unique()
+pcor<-sapply(res2par, function(x){
+  attributes(x)$pcor
+} ) %>% unique()
+p<-sapply(res2par, function(x){
+  attributes(x)$p
+} ) %>% unique()
+ratio_beta<-lapply(res2par, function(x){
+  attributes(x)$ratio_beta
+} ) %>% unique()
+
+att<-list(n=n,
+          studies=studies,
+          iterations=iterations,
+          hypothesis=hypothesis,
+          pop_names=pop_names,
+          r2=r2,
+          pcor=pcor,
+          p=p,
+          ratio_beta=ratio_beta
+)
+#reshape the inner lists to 4d arrays where [studies, BF_hyp, iter, n],  i.e.
+#   -studies*iterations are split to different dimensions
+#   -levels of n are put into a dimension
+# => array4d is a list condtaining 4d arrays of lenght the number of populations
+array4d<-lapply(res2par, function(x){
+  reshapeBFs(BF_list=x, # a list, where each element (a dataframe, col=hypotheses, rows=iterations*studies) contains the BFs for a certain sample size n
+             n=n,
+             studies=studies,
+             iterations=iterations)
+})
+
+# #save attributes of the resulting arrays 
+# #again, they are the same across populations
+# att<-attributes(array4d[[1]])
+# #remove the attributes about dimensionality, and keep only the attributes relevant to the simulation conditions
+# att<-att[names(att)[-grep("dim", names(att))]]
+# #remove the "names" attribute
+# att<-att[names(att)[-grep("names", names(att))]]
+
+
+#bind the 4d arrays (i.e., populations) along the 5th dim
+BF_bind<-do.call(what = "abind", args=list(array4d, along=5))%>% 
+  #and reorder the dims such that the structure is [t,BF_hyp, pop, iter,n]
+  aperm(perm=c(1,2,5,3,4)) 
+
+#name the populations contained in teh 3th dim
+dimnames(BF_bind)[[3]]<-pop_names
+
+#add BFuu = 1
+#First, create an array slice for BFuu
+Hu_array<-array(1, dim = c(studies,
+                           1,
+                           length(pop_names),
+                           iterations,
+                           length(n)
+))
+#combine the array slicefor BFuu with the main array
+BF_bind2<-abind(BF_bind, Hu_array, along = 2)
+#give the BFuu slice a name "Hu"
+dimnames(BF_bind2)[[2]][length(dimnames(BF_bind2)[[2]])]<-"Hu"
+
+attributes(BF_bind2)<-c(attributes(BF_bind2),
+                        att)
+
+saveRDS(BF_bind2, file="~/pre-processing/output/BF_data_2par.rds")
+
+
+#n_par 1 -----------------------------------------
+
+res1par<-readRDS(file= "~/simulation/output//res1par.rds")
+
+
+
+#Simulation conditions
+# I use only the attributes from one of the populations, because they are the same across populations
+n = attributes(res1par[[1]])$n
+studies<-attributes(res1par[[1]])$studies
+iterations=attributes(res1par[[1]])$iterations
+hypothesis<-attributes(res1par[[1]])$hypothesis
+#all populations
+pop_names<-sapply(res1par, function(x){
+  attributes(x)$pop_name
+} )
+r2<-sapply(res1par, function(x){
+  attributes(x)$r2
+} ) %>% unique()
+pcor<-sapply(res1par, function(x){
+  attributes(x)$pcor
+} ) %>% unique()
+p<-sapply(res1par, function(x){
+  attributes(x)$p
+} ) %>% unique()
+ratio_beta<-lapply(res1par, function(x){
+  attributes(x)$ratio_beta
+} ) %>% unique()
+
+att<-list(n=n,
+          studies=studies,
+          iterations=iterations,
+          hypothesis=hypothesis,
+          pop_names=pop_names,
+          r2=r2,
+          pcor=pcor,
+          p=p,
+          ratio_beta=ratio_beta
+)
+#reshape the inner lists to 4d arrays where [studies, BF_hyp, iter, n],  i.e.
+#   -studies*iterations are split to different dimensions
+#   -levels of n are put into a dimension
+# => array4d is a list condtaining 4d arrays of lenght the number of populations
+array4d<-lapply(res1par, function(x){
+  reshapeBFs(BF_list=x, # a list, where each element (a dataframe, col=hypotheses, rows=iterations*studies) contains the BFs for a certain sample size n
+             n=n,
+             studies=studies,
+             iterations=iterations)
+})
+
+# #save attributes of the resulting arrays 
+# #again, they are the same across populations
+# att<-attributes(array4d[[1]])
+# #remove the attributes about dimensionality, and keep only the attributes relevant to the simulation conditions
+# att<-att[names(att)[-grep("dim", names(att))]]
+# #remove the "names" attribute
+# att<-att[names(att)[-grep("names", names(att))]]
+# 
+
+#bind the 4d arrays (i.e., populations) along the 5th dim
+BF_bind<-do.call(what = "abind", args=list(array4d, along=5))%>% 
+  #and reorder the dims such that the structure is [t,BF_hyp, pop, iter,n]
+  aperm(perm=c(1,2,5,3,4)) 
+
+#name the populations contained in teh 3th dim
+dimnames(BF_bind)[[3]]<-pop_names
+
+#add BFuu = 1
+#First, create an array slice for BFuu
+Hu_array<-array(1, dim = c(studies,
+                           1,
+                           length(pop_names),
+                           iterations,
+                           length(n)
+))
+#combine the array slicefor BFuu with the main array
+BF_bind1<-abind(BF_bind, Hu_array, along = 2)
+#give the BFuu slice a name "Hu"
+dimnames(BF_bind1)[[2]][length(dimnames(BF_bind1)[[2]])]<-"Hu"
+
+attributes(BF_bind1)<-c(attributes(BF_bind1),
+                        att)
+
+saveRDS(BF_bind1, file="~/pre-processing/output/BF_data_1par.rds")
---
+++ scripts for hpc/simulation functions.R
@@ -0,0 +1,316 @@
+library(tidyverse)
+library(magrittr)
+#library(furrr)
+library(MASS)
+#library(mvtnorm)
+#library(bain)
+library(parallel)
+# library(ggplot2)
+# library(ggstatsplot)
+# library(plotly)
+# library(readxl)
+# library(writexl)
+# library(lavaan)
+# library(devtools)
+# library(SSDbain)
+# library(jtools)
+# library(ggrepel)
+library(BFpack)
+# library(Rcpp)
+# library(RcppArmadillo)
+
+cormat <- function(partial_cor, diag_length) {
+  r <- diag(diag_length)    # fill correlation matrix with partial correlation
+  r[r != 1] <- partial_cor  # between variables
+  r
+}
+
+coefs <- function(r2, ratio, rho, model = c("normal", "logit", "probit")) {
+  
+  # variance of predicted values (Var(yhat))
+  if (model == "normal") {
+    var_y <- r2
+  }
+  else if (model == "logit") {
+    var_y <- (r2 * pi^2 / 3) / (1 - r2)
+  }
+  else if (model == "probit") {
+    var_y <- r2 / (1 - r2)
+  }
+  # value of the regression coefficients
+  sqrt(var_y / sum(ratio %*% t(ratio) * rho)) * ratio
+}
+
+gen_dat <- function(r2, betas, rho, n, model = c("normal", "logit", "probit"), 
+                    mutate_args = NULL, select_args = quos(everything())) {
+  # generate predictors X
+  X <- mvrnorm(n, mu = rep(0, length(betas)), Sigma = rho)
+  
+  # generate outcome variable Y
+  if (model == "normal") {
+    Y <- X %*% betas + rnorm(n = n, mean = 0, sd = sqrt(1 - r2))
+  }
+  if (model == "logit") {
+    Y <- rbinom(n, 1, 1 / (1 + exp(-(X %*% betas))))
+  }
+  if (model == "probit") {
+    Y <- rbinom(n, 1, pnorm(X %*% betas))
+  }
+  # output data
+  bind_cols(X = as.data.frame(X), Y = Y) #%>%
+    # mutate(!!!mutate_args) %>%
+    # select(!!!select_args)
+}
+
+q_glm <- quietly(glm)
+
+
+#a function to simulate lm model according to certain simulation conditions
+single_sim<-function(r2,  # R-squared of the regression model
+                     pcor,  # correlation between the predictors
+                     ratio_beta,  # a numeric vector with the true beta parameters (or the means of the mvnorm dist, if they are sampled);
+                     # Sigma_beta,  # variance covariance matrix of the (true) regression parameters - can be used to induce heterogeneity
+                     p=0.6829787, #the proportion of the effect size used to determine the SD of the parameters: SD=propSD*betas, 70% is a wide distribution (REF)
+                     hypothesis=c( "V1>V2>V3"),
+                     n,  #sample size 
+                     model  #linear, logistic or probit regression
+){
+  #n.hyp<-length(unlist(strsplit(hypothesis, ";")))
+  n.hyp<-1 # for now I'd limit the user to only testing a single hypothesis
+  
+  #sample new ratios from a normal distribution
+  new_ratio<-mvrnorm(1,mu=ratio_beta, Sigma=diag(p*ratio_beta))
+  #resample every time any of the sampled values is negative (because for hte simulation conditions the ratios must be positive)
+  while(any(new_ratio<0)) {
+    new_ratio<-mvrnorm(1,mu=ratio_beta, Sigma=diag(p*ratio_beta))
+  }
+  
+  betas<-coefs(r2, new_ratio, cormat(pcor, length(new_ratio)), "normal")
+  
+  #obtain lm object
+  lm.mod<-gen_dat(r2=r2,
+                  betas=betas,
+                  rho=cormat(pcor, length(betas)),
+                  n=n,
+                  model="normal") %>% 
+    lm(Y~., data=.)
+  
+  varnames<-paste0("H", 1:length(hypothesis))
+  
+  for(h in 1:length(varnames)){
+    
+    assign(varnames[h],  BF(lm.mod, hypothesis=hypothesis[h], complement=TRUE)$BFtable_confirmatory %>%
+             as.data.frame() %>% 
+             rownames_to_column() %>% 
+             pull(var=BF, name = rowname)
+           )
+  }
+  
+  BFs<-unlist(mget(varnames))
+  return(BFs)
+}
+
+
+#a function to simulate lm model according to certain simulation conditions
+#sample ratios from log-normal distribution instead of normal with replacement for negative values
+single_sim_ln<-function(r2,  # R-squared of the regression model
+                     pcor,  # correlation between the predictors
+                     ratio_beta=c(3,2,1),  # a numeric vector with the true beta parameters (or the means of the mvnorm dist, if they are sampled);
+                     # Sigma_beta,  # variance covariance matrix of the (true) regression parameters - can be used to induce heterogeneity
+                     p=0.86, #the proportion of the effect size used to determine the SD of the parameters: SD=propSD*betas, 70% is a wide distribution (REF)
+                     hypothesis=c( "V1>V2>V3"),
+                     n,  #sample size 
+                     model  #linear, logistic or probit regression
+){
+  #n.hyp<-length(unlist(strsplit(hypothesis, ";")))
+  n.hyp<-1 # for now I'd limit the user to only testing a single hypothesis
+  
+  #sample new ratios from a log-normal distribution  
+  #with mean = ratio, and sd=p*ratio
+  m<-ratio_beta
+  s=p*ratio_beta
+  # in order to draw from a log-normal dist with these mean and sd
+  # the location and shape parameters must be reparametrized
+  #https://msalganik.wordpress.com/2017/01/21/making-sense-of-the-rlnorm-function-in-r/comment-page-1/
+  location <- log(m^2 / sqrt(s^2 + m^2))
+  shape <- sqrt(log(1 + (s^2 / m^2)))
+  
+  new_ratio<-c()
+  for(r in 1:length(ratio_beta)){
+    new_ratio[r]<-rlnorm(n=1, location[r], shape[r])
+  }
+  
+  betas<-coefs(r2, new_ratio, cormat(pcor, length(new_ratio)), "normal")
+  
+  #obtain lm object
+  lm.mod<-gen_dat(r2=r2,
+                  betas=betas,
+                  rho=cormat(pcor, length(betas)),
+                  n=n,
+                  model="normal") %>% 
+    lm(Y~., data=.)
+  
+  varnames<-paste0("H", 1:length(hypothesis))
+  
+  for(h in 1:length(varnames)){
+    
+    assign(varnames[h],  BF(lm.mod, hypothesis=hypothesis[h], complement=TRUE)$BFtable_confirmatory %>%
+             as.data.frame() %>% 
+             rownames_to_column() %>% 
+             pull(var=BF, name = rowname)
+    )
+  }
+  
+  BFs<-unlist(mget(varnames))
+  return(BFs)
+}
+
+#parallel
+run_sim_ln<-function(r2=0.13,
+                  pcor=0.3,
+                  hypothesis="V1>V2>V3",
+                  ratio_beta=c(3,2,1),
+                  p=0,
+                  n = c(50,75,100,150,200,300),
+                  model="linear",
+                  studies=30,
+                  iterations=1000,
+                  ncores=7,
+                  seed){
+  
+  
+  # print( paste0("Prep cluster: ",Sys.time()))
+  # cl<-makeCluster(ncores)
+  # clusterSetRNGStream(cl, seed)
+  # 
+  # clusterEvalQ(cl, {
+  #   library(MASS)
+  #   library(magrittr)
+  #   library(tidyverse)
+  #   library(BFpack)
+  # })
+  # clusterExport(cl=cl, c("single_sim_ln", "q_glm", "gen_dat", "cormat", "coefs"))
+  # 
+  # clusterExport(cl=cl, varlist=c("n", "r2", "pcor", "ratio_beta", "hypothesis",
+  #                                "p","model"),envir = environment())
+  # 
+  # print(paste0("Start sim: ",Sys.time()))
+  # BF_list<- 
+  #   lapply(1:length(n), function(s){
+  #     print(paste0("Sample size: ",s, ";", Sys.time()))
+  #     t(parSapply(cl, 1:(iterations*studies), function(i){
+  #       single_sim_ln(r2=0.13,
+  #                  pcor=pcor,
+  #                  hypothesis=hypothesis,
+  #                  ratio_beta=ratio_beta,
+  #                  p=p,
+  #                  n = n[s],
+  #                  model=model)
+  #       
+  #     }))
+  #   })
+  # stopCluster(cl)
+  # print(paste0("End sim: ",Sys.time()))
+  
+  BF_list<- 
+    lapply(1:length(n), function(s){
+      print(paste0("Sample size: ",s, ";", Sys.time()))
+      t(sapply(1:(iterations*studies), function(i){
+        single_sim_ln(r2=0.13,
+                      pcor=pcor,
+                      hypothesis=hypothesis,
+                      ratio_beta=ratio_beta,
+                      p=p,
+                      n = n[s],
+                      model=model)
+        
+      }))
+    })
+  
+  BF_list<-lapply(BF_list, function(x) {
+    cbind(x,1)
+    return(x)
+  } )
+  
+  attributes(BF_list)<-list(hypothesis=hypothesis,
+                            complexity="check",
+                            r2=r2,
+                            pcor=pcor,
+                            ratio_beta=ratio_beta,
+                            p=0,
+                            model="linear",
+                            seed=seed,
+                            iterations=iterations,
+                            studies=studies,
+                            n=n)
+  
+  return(BF_list)
+} #end run_sim()
+
+#parallel
+run_sim<-function(r2=0.13,
+                  pcor=0.3,
+                  hypothesis="V1>V2>V3",
+                  ratio_beta=c(3,2,1),
+                  p=0,
+                  n = c(50,75,100,150,200,300),
+                  model="linear",
+                  studies=30,
+                  iterations=1000,
+                  ncores=7,
+                  seed){
+  
+  
+  print( paste0("Prep cluster: ",Sys.time()))
+  
+  cl<-makeCluster(ncores)
+  clusterSetRNGStream(cl, seed)
+  
+  clusterEvalQ(cl, {
+    library(MASS)
+    library(magrittr)
+    library(tidyverse)
+    library(BFpack)
+  })
+  clusterExport(cl=cl, c("single_sim", "q_glm", "gen_dat", "cormat", "coefs"))
+  
+  clusterExport(cl=cl, varlist=c("n", "r2", "pcor", "ratio_beta", "hypothesis",
+                         "p","model"),envir = environment())
+  
+  print(paste0("Start sim: ",Sys.time()))
+  BF_list<- 
+    lapply(1:length(n), function(s){
+      print(paste0("Sample size: ",s, ";", Sys.time()))
+      t(parSapply(cl, 1:(iterations*studies), function(i){
+        single_sim(r2=0.13,
+                   pcor=pcor,
+                   hypothesis=hypothesis,
+                   ratio_beta=ratio_beta,
+                   p=p,
+                   n = n[s],
+                   model=model)
+        
+      }))
+    })
+  print(paste0("End sim: ",Sys.time()))
+  stopCluster(cl)
+  
+  BF_list<-lapply(BF_list, function(x) {
+    cbind(x,1)
+    return(x)
+  } )
+  
+  attributes(BF_list)<-list(hypothesis=hypothesis,
+                            complexity="check",
+                            r2=r2,
+                            pcor=pcor,
+                            ratio_beta=ratio_beta,
+                            p=0,
+                            model="linear",
+                            seed=seed,
+                            iterations=iterations,
+                            studies=studies,
+                            n=n)
+  
+  return(BF_list)
+} #end run_sim()
