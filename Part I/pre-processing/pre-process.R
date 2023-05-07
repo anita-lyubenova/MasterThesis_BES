2cfb88e31788790a313f4320f2a2e43e88bc92a6
anita-lyubenova <anitaencheva@gmail.com>
2023-05-07 18:49:20 +0200

    preprocess final hpc data for 2 parameters

--- Part I/pre-processing/pre-process.R
+++ Part I/pre-processing/pre-process.R
@@ -685,3 +685,135 @@ attributes(BF_bind4)<-c(attributes(BF_bind4),
 saveRDS(BF_bind4, file="Part I/pre-processing/output/BF_data_3par_hpc_final_mixed.rds")
 
 
+
+# 2 predictors -----------------------------
+library(dplyr)
+library(abind)
+
+source("Part I/pre-processing/pre-processing functions.R")
+
+res2par<-readRDS(file= "Part I/simulation/output/res2par_shiny.rds")
+length(res2par)
+dimnames(res2par[[1]])[[2]]
+res2par[[2]]
+attributes(res2par[[1]])
+##Step 1) XXX ------------------------
+
+pop_names<-sapply(res2par, function(x){return(attributes(x)$pop_name)}) 
+
+rats<-gsub(".*_b(.+)_p.*", "\\1", pop_names) %>% 
+ gsub('3', '', .)
+
+pop_names<-paste0(str_extract(pop_names, ".+?(?<=_b)"), rats, str_extract_all(pop_names, "_p\\d.*"))
+names(res2par)<-pop_names
+
+## Step 2) nest all sample sizes of a population in an inner list -----------------------
+#get the names of the populations
+pop<-sub(pattern="_n.*", "", pop_names)  %>% unique()
+n<-sub(pattern=".*_n", "", pop_names)  %>% unique()
+
+res2par2<-lapply(pop, function(s){
+  x<-res2par[sub(pattern="_n.*", "", names(res2par))==s]
+  names(x)<-n
+  return(x)
+})
+
+names(res2par2)<-pop
+
+
+## Step 3) apply pre-processing procedure "Version 2" to res3par2------------
+
+#Simulation conditions
+# I use only the attributes from one of the populations, because they are the same across populations
+n = n
+studies<-30
+iterations=1000
+hypothesis<-dimnames(res2par[[1]])[[2]]
+#all populations
+pop_names<-sub(pattern="_n.*", "", pop_names) %>% unique()
+r2<- gsub(".*r(.+)_pcor.*", "\\1", pop_names) %>% unique()
+pcor<-gsub(".*_pcor(.+)_b.*", "\\1", pop_names) %>% unique()
+p<-gsub(pattern = ".*p", "", pop_names) %>% unique()
+ratio_beta<- gsub(".*_b(.+)_p.*", "\\1", pop_names) %>% unique()
+
+att<-list(n=n,
+          studies=studies,
+          iterations=iterations,
+          hypothesis=hypothesis,
+          pop_names=pop_names,
+          r2=r2,
+          pcor=pcor,
+          p=p,
+          ratio_beta=ratio_beta
+)
+#reshape the inner lists to 4d arrays where [studies, BF_hyp, iter, n],  i.e.
+#   -studies*iterations are split to different dimensions
+#   -levels of n are put into a dimension
+# => array4d is a list condtaining 4d arrays of lenght the number of populations
+array4d<-lapply(res2par2, function(x){
+  reshapeBFs(BF_list=x, # a list, where each element (a dataframe, col=hypotheses, rows=iterations*studies) contains the BFs for a certain sample size n
+             n=n,
+             studies=studies,
+             iterations=iterations)
+})
+
+# #save attributes of the resulting arrays that are to be used for the final data
+# #again, they are the same across populations
+# att<-attributes(array4d[[1]])
+# #remove the attributes about dimensionality, and keep only the attributes relevant to the simulation conditions
+# att<-att[names(att)[-grep("dim", names(att))]]
+# #remove the "names" attribute
+# att<-att[names(att)[-grep("names", names(att))]]
+
+
+#bind the 4d arrays (i.e., populations) along the 5th dim
+BF_bind<-do.call(what = "abind", args=list(array4d, along=5))%>%
+  #and reorder the dims such that the structure is [t,BF_hyp, pop, iter,n]
+  aperm(perm=c(1,2,5,3,4))
+
+
+#name the populations contained in teh 3th dim
+dimnames(BF_bind)[[3]]<-pop_names
+
+#add BFuu = 1
+#First, create an array slice for BFuu
+Hu_array<-array(1, dim = c(studies,
+                           1,
+                           length(pop_names),
+                           iterations,
+                           length(n)
+))
+#combine the array slicefor BFuu with the main array
+BF_bind3<-abind(BF_bind, Hu_array, along = 2)
+#give the BFuu slice a name "Hu"
+dimnames(BF_bind3)[[2]][length(dimnames(BF_bind3)[[2]])]<-"Hu"
+
+
+###add mixed population H1+Hc -----------------------
+
+mixed_pop<-array(NA, dim = c(studies,
+                             length(dimnames(BF_bind3)[[2]]), #number of hypotheses
+                             3*3*6,
+                             iterations,
+                             length(n)
+))
+dimnames(mixed_pop)[[3]]<-1:(3*3*6)
+dim(mixed_pop)
+# i<-1
+# s<-1
+# t<-1
+# y<-0.09
+# z<-0
+# j<-0
+# x<-1
+
+
+mixed_pop[,,1:9,,] %>% dim
+#combine the array slice with the main arrays
+BF_bind4<-abind(BF_bind3, mixed_pop[,,1:9,,], along = 3)
+
+
+attributes(BF_bind4)<-c(attributes(BF_bind4),
+                        att)
+
+saveRDS(BF_bind4, file="Part I/pre-processing/output/BF_data_2par_hpc_final_mixed.rds")
