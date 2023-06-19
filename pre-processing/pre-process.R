80de48c560af3ae1d1409f9f56902956554e457e
anita-lyubenova <anitaencheva@gmail.com>
2023-06-19 12:29:46 +0200

    process simulated data with 1 categorical predictor

--- pre-processing/pre-process.R
+++ pre-processing/pre-process.R
@@ -17,18 +17,23 @@ library(abind)
 
 source("pre-processing/pre-processing functions.R")
 
-res1par<-readRDS(file= "simulation/output/res1par.rds")
+res1par<-readRDS(file= "simulation/output/BFresults.rds")
 length(res1par)
 dimnames(res1par[[1]])[[2]]
-res1par[[2]]
 
 ##Step 1) Name each element of the list ------------------------
 
-pop_names<-sapply(res1par, function(x){return(attributes(x)$pop_name)}) %>% 
-  str_remove( "b_")
+pop_names<-sapply(res1par, function(x){return(attributes(x)$pop_name)}) 
 names(res1par)<-pop_names
 
-#Output: the same list with 135 elements, but the elements are named
+res1par<-lapply(res1par, function(x){
+  x[,"Hu"]<-1
+  Hc <- 2-x[,"H1"]
+  x<-cbind(x,Hc)
+  x[,c("H1", "Hc", "Hu")]
+})
+
+#Output: the same list with 135 elements, but the elements are named; and BFcu was added
 
 ## Step 2) nest all sample sizes of the same population in an inner list -----------------------
 #get the names of the populations and sample sizes
@@ -38,36 +43,40 @@ n<-sub(pattern=".*_n", "", pop_names)  %>% unique()
 res1par2<-lapply(pop, function(s){
   x<-res1par[sub(pattern="_n.*", "", names(res1par))==s]
   names(x)<-n
+
   return(x)
 })
 
 names(res1par2)<-pop
 
-str(res1par2)
+############################add BFcu
+res1par2[[2]]
+
+####################
 #Output:
 # a nested list with:
-#     15 elements at level 1 (conditions) [beta_mu x beta_tau];
-#     9  elements at level 2 [sample sizes n], where each element is a dataframe [60000, 2] 
+#     16 elements at level 1 (conditions) [delta x tau];
+#     8  elements at level 2 [sample sizes n], where each element is a dataframe [60000, 2] 
 
 ## Step 3) apply pre-processing procedure "Version 2" to res1par2------------
 
 #save simulation conditions to a list to later use as attributes
 
-studies<-60
+studies<-30
 iterations=1000
 hypothesis<-dimnames(res1par[[1]])[[2]]
 #all populations
 pop_names<-sub(pattern="_n.*", "", pop_names) %>% unique()
-beta_mu<- gsub(".*beta_mu(.+)_beta_tau.*", "\\1", pop_names) %>% unique()
-beta_tau<-gsub(pattern = ".*_beta_tau", "", pop_names) %>% unique()
+delta<- gsub(".*delta(.+)_tau.*", "\\1", pop_names) %>% unique()
+tau<-gsub(pattern = ".*_tau", "", pop_names) %>% unique()
 
 att<-list(n=n,
           studies=studies,
           iterations=iterations,
           hypothesis=hypothesis,
           pop_names=pop_names,
-          beta_mu=beta_mu,
-          beta_tau=beta_tau
+          delta=delta,
+          tau=tau
 )
 #reshape the inner lists (level 2) to 4d arrays where the dimensions are [studies, BF_hyp, iter, n],  i.e.
 #   -studies*iterations are split to different dimensions
@@ -92,18 +101,18 @@ dim(BF_bind)
 #name the populations contained in teh 3th dim
 dimnames(BF_bind)[[3]]<-pop_names
 
-#add BFuu = 1
-#First, create an array slice for BFuu
-Hu_array<-array(1, dim = c(studies,
-                           1,
-                           length(pop_names),
-                           iterations,
-                           length(n)
-))
-#combine the array slicefor BFuu with the main array
-BF_bind3<-abind(BF_bind, Hu_array, along = 2)
-#give the BFuu slice a name "Hu"
-dimnames(BF_bind3)[[2]][length(dimnames(BF_bind3)[[2]])]<-"Hu"
+# #add BFuu = 1
+# #First, create an array slice for BFuu
+# Hu_array<-array(1, dim = c(studies,
+#                            1,
+#                            length(pop_names),
+#                            iterations,
+#                            length(n)
+# ))
+# #combine the array slicefor BFuu with the main array
+# BF_bind3<-abind(BF_bind, Hu_array, along = 2)
+# #give the BFuu slice a name "Hu"
+# dimnames(BF_bind3)[[2]][length(dimnames(BF_bind3)[[2]])]<-"Hu"
 
 
 ###add mixed population H1+Hc -----------------------
@@ -166,8 +175,8 @@ dimnames(BF_bind3)[[2]][length(dimnames(BF_bind3)[[2]])]<-"Hu"
 # BF_bind4<-abind(BF_bind3, mixed_pop[,,1:9,,], along = 3)
 
 
-attributes(BF_bind3)<-c(attributes(BF_bind3),
+attributes(BF_bind)<-c(attributes(BF_bind),
                         att)
 
-saveRDS(BF_bind3, file="pre-processing/output/BF_data_1par.rds")
+saveRDS(BF_bind, file="pre-processing/output/processed_data.rds")
 
