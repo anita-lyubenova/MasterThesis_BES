b0976f15c8c51b1a56ef11654e14506f28d3b9dd
anita-lyubenova <anitaencheva@gmail.com>
2023-06-22 14:06:13 +0200

    add delta=-0.2 to the main simulation

--- pre-processing/pre-process.R
+++ pre-processing/pre-process.R
@@ -11,7 +11,7 @@
 ################################################################################################## 2
 
 
-# 3 predictors -----------------------------
+# 1 predictor -----------------------------
 library(dplyr)
 library(abind)
 
@@ -180,3 +180,135 @@ attributes(BF_bind)<-c(attributes(BF_bind),
 
 saveRDS(BF_bind, file="pre-processing/output/processed_data.rds")
 
+
+# delta = -0.2 --------------------------------------------------------------
+res <- readRDS("simulation/output/BFresults_delta-0.2.rds")
+
+##Step 1) Name each element of the list ------------------------
+
+pop_names<-sapply(res, function(x){return(attributes(x)$pop_name)}) 
+names(res)<-pop_names
+
+#add Hc
+res<-lapply(res, function(x){
+  x[,"Hu"]<-1
+  Hc <- 2-x[,"H1"]
+  x<-cbind(x,Hc)
+  x[,c("H1", "Hc", "Hu")]
+})
+
+#Output: the same list with 135 elements, but the elements are named; and BFcu was added
+
+## Step 2) nest all sample sizes of the same population in an inner list -----------------------
+#get the names of the populations and sample sizes
+pop<-sub(pattern="_n.*", "", pop_names)  %>% unique()
+n<-sub(pattern=".*_n", "", pop_names)  %>% unique()
+
+res2<-lapply(pop, function(s){
+  x<-res[sub(pattern="_n.*", "", names(res))==s]
+  names(x)<-n
+  
+  return(x)
+})
+
+names(res2)<-pop
+
+############################add BFcu
+res2[[2]]
+
+####################
+#Output:
+# a nested list with:
+#     16 elements at level 1 (conditions) [delta x tau];
+#     8  elements at level 2 [sample sizes n], where each element is a dataframe [60000, 2] 
+
+## Step 3) apply pre-processing procedure "Version 2" to res2------------
+
+#save simulation conditions to a list to later use as attributes
+
+studies<-30
+iterations=1000
+hypothesis<-dimnames(res[[1]])[[2]]
+#all populations
+pop_names<-sub(pattern="_n.*", "", pop_names) %>% unique()
+delta<- gsub(".*delta(.+)_tau.*", "\\1", pop_names) %>% unique()
+tau<-gsub(pattern = ".*_tau", "", pop_names) %>% unique()
+
+att<-list(n=n,
+          studies=studies,
+          iterations=iterations,
+          hypothesis=hypothesis,
+          pop_names=pop_names,
+          delta=delta,
+          tau=tau
+)
+#reshape the inner lists (level 2) to 4d arrays where the dimensions are [studies, BF_hyp, iter, n],  i.e.
+#   -studies*iterations are split to different dimensions
+#   -levels of n are put into a dimension
+
+array4d<-lapply(res2, function(x){
+  reshapeBFs(BF_list=x, # a list, where each element (a dataframe, col=hypotheses, rows=iterations*studies) contains the BFs for a certain sample size n
+             n=n,
+             studies=studies,
+             iterations=iterations)
+})
+# => array4d is a list of length 15 [number of conditions], where each element is a 4d array
+
+
+
+#bind the 4d arrays within the list (i.e., populations) along the 5th dim
+BF_bind<-do.call(what = "abind", args=list(array4d, along=5))%>%
+  #and reorder the dims such that the structure is [t,BF_hyp, population, iter,n]
+  aperm(perm=c(1,2,5,3,4))
+dim(BF_bind)
+
+#name the populations contained in teh 3th dim
+dimnames(BF_bind)[[3]]<-pop_names
+
+# #add BFuu = 1
+# #First, create an array slice for BFuu
+# Hu_array<-array(1, dim = c(studies,
+#                            1,
+#                            length(pop_names),
+#                            iterations,
+#                            length(n)
+# ))
+# #combine the array slicefor BFuu with the main array
+# BF_bind3<-abind(BF_bind, Hu_array, along = 2)
+# #give the BFuu slice a name "Hu"
+# dimnames(BF_bind3)[[2]][length(dimnames(BF_bind3)[[2]])]<-"Hu"
+
+
+
+attributes(BF_bind)<-c(attributes(BF_bind),
+                       att)
+
+saveRDS(BF_bind, file="pre-processing/output/processed_data_delta-0.2.rds")
+
+
+# COMBINE --------------------------------------------------------------------
+main<-readRDS(file= "pre-processing/output/processed_data.rds")
+d2<-readRDS(file="pre-processing/output/processed_data_delta-0.2.rds")
+
+a<-attributes(main)
+b<-attributes(d2)
+
+comb<-abind(main,d2, along = 3)
+dimnames(comb)[[3]]
+attributes(comb)
+
+attributes(main)[!grepl("dim", names(attributes(main)))]
+
+cattr<-list(n=a$n,
+            studies=a$studies,
+            iterations=a$iterations,
+            hypothesis=a$hypothesis,
+            pop_names=dimnames(comb)[[3]],
+            delta=c(a$delta, b$delta),
+            tau=a$tau
+            )
+
+attributes(comb)<-c(attributes(comb),
+                       cattr)
+
+saveRDS(comb, file="pre-processing/output/processed_data_combined.rds")
