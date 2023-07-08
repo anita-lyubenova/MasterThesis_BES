4134488287650096cb3344a9f7c2b0eb2794b0f9
anita-lyubenova <anitaencheva@gmail.com>
2023-07-08 11:38:59 +0200

    Update troubleshoot aggregation.R

--- analysis/troubleshoot aggregation.R
+++ analysis/troubleshoot aggregation.R
@@ -5,3 +5,57 @@ dat<-readRDS("pre-processing/output/processed_data_combined.rds")
 dimnames(dat)[[3]]
 pops<-dimnames(dat)[[3]]
 tau<-attributes(dat)$tau
+
+x=dat
+hyp=c("H1", "Hc")
+studies=30
+
+#a function to compute aggregate PMPs for selected hypotheses from the BFs
+aggregatePMP<-function(x, # a 5 dim array with structure [t, BF, pop, iter, n]
+                       hyp=c("H1", "Hu"),
+                       studies=10, #number of studies to aggregate over, max 40,
+                       subset=NULL
+){
+  # #subset if specified, while retaining the attributes
+  # if(!is.null(subset)){
+  #   att<-attributes(x)
+  #   att<-att[names(att)[-grep("dim", names(att))]]
+  #   x<-eval(parse(text = subset))
+  #   attributes(x)<-c(attributes(x), att)
+  # }
+  # 
+  hyp_index<-substr(hyp,2,2)
+  search_terms <- paste0(hyp, collapse = "|")
+  BF<-x
+  BF<-BF[1:studies,str_subset(dimnames(BF)[[2]], search_terms),,,,drop=FALSE]
+  
+  nom<-aperm(BF, perm=c(1,3,4,5,2))
+  denom<-rowSums(nom, dims = 4)
+  PMP_perm<-nom/replicate(length(hyp),denom)
+  
+  #placeholder for tha aggregated PMPs
+  PMP_t<-PMP_perm
+  for(t in 2:studies){ #the PMPs of the first study remain the same, thus iterate from t=2
+    nom_t<-PMP_t[t-1,,,,]*nom[t,,,,]#multiply the previous PMPs with the current BFs
+    denom_t<-rowSums(nom_t, dims=3)
+    PMP_t[t,,,,]<-nom_t/replicate(length(hyp), denom_t)
+  }
+  
+  PMP_t<-aperm(PMP_t, perm = c(1,5,2,3,4))
+  dimnames(PMP_t)[[2]]<-paste0("PMP", c(hyp_index))
+  
+  PMP_t<-  rlist::list.append(attributes(x), PMP=PMP_t, hypothesis_test = paste(hyp, collapse = " vs. ")) 
+  return(PMP_t)
+} # end aggregatePMP
+
+sum(dat[30,"H1","delta0_tau0.45" ,,"300"] >0.5)
+
+
+
+
+
+
+
+
+
+
